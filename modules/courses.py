5744aa4037f5833d8b7da8be5f0de772025ae686
Cassidy <cbain919@gmail.com>
2026-03-08 20:24:33 -0400

    Improve course autocompletion

--- modules/courses.py
+++ modules/courses.py
@@ -66,8 +66,7 @@ class Course:
 
     def closeness(self, user_input: str):
         term = user_input.lower()
-        return 3 if term in self.abbrev.lower() else 2 if term in self.name.lower() else \
-            1 if term in self.full_display.lower() else 0
+        return closeness(term, self.abbrev.lower()) * 2 + closeness(term, self.name.lower())
 
 
 courses: dict[int, Course] = {
